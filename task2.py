feb8accd58a1812102901852661081b25793784b
mainak890 <138385062+mainak890@users.noreply.github.com>
2025-12-07 11:56:35 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,8 @@
+import math
+x = float(input("Enter a number: "))
+square_root = math.sqrt(x)
+natural_log = math.log(x)
+sine_value = math.sin(x)
+print("Square Root: ", square_root)
+print("Natural Logarithm: ", natural_log)
+print("Sine: ", sine_value)
